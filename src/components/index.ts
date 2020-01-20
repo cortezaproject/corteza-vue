914fe765070472ce53bdff454a833eee145503d5
Denis Arh <denis.arh@gmail.com>
2020-01-20 20:26:49 +0100

    Cleanup component registration

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,3 @@
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 import {
   PermissionsButton,
   PermissionsForm,
@@ -11,9 +7,6 @@ import {
 import {
   CorredorManualButtons,
 } from './corredor'
-
-Vue.use(BootstrapVue)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 export {
   PermissionsButton,
