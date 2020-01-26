a42445f423ccb47fac798d9bdc5d3075c9fd4ce6
Denis Arh <denis.arh@gmail.com>
2020-01-26 14:19:13 +0100

    Pass plugin reg. options to eventbus

--- src/plugins/eventbus.ts
+++ src/plugins/eventbus.ts
@@ -1,8 +1,8 @@
 import { PluginFunction } from 'vue'
 import { EventBus } from '@cortezaproject/corteza-js'
 
-export default function (): PluginFunction<object> {
+export default function (options: object): PluginFunction<object> {
   return function (Vue): void {
-    Vue.prototype.$EventBus = new EventBus()
+    Vue.prototype.$EventBus = new EventBus(options)
   }
 }
