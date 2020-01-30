299e27aa1aad7a7a3506562e46435c35b9301ab8
Denis Arh <denis.arh@gmail.com>
2020-01-30 01:17:45 +0100

    Export URL helper utility

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import * as plugins from './plugins'
 import * as mixins from './mixins'
 import * as components from './components'
 import * as filters from './filters'
+import * as url from './libs/url'
 
 export {
   plugins,
   mixins,
   components,
   filters,
+  url,
 }
