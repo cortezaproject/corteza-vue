aa117fc5f6ef2c87b64cbd7bbcc426d6b88c1bdb
Denis Arh <denis.arh@gmail.com>
2020-02-16 14:49:15 +0100

    Update initial bundle setup procedure

--- src/mixins/corredor.js
+++ src/mixins/corredor.js
@@ -1,6 +1,14 @@
+import { corredor } from '@cortezaproject/corteza-js'
+
 /**
  * Corredor automation mixin
  */
+
+/**
+ * With this prefix we distinguish script type
+ * @type {string}
+ */
+const serverScriptPrefix = '/server-scripts/'
 
 export default {
   methods: {
@@ -9,7 +17,7 @@ export default {
      *
      * API should be corteza API Client class that is passed as a first arg to serverScriptHandler
      */
-    makeAutomationScriptsRegistrator (api, serverScriptHandler) {
+    makeAutomationScriptsRegistrator (serverScriptHandler) {
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       return ({ set }) => {
         /**
@@ -24,7 +32,7 @@ export default {
          *  2. onManual only -- other kinds (implicit, deferred) are handled directly in/by the Corteza API backend
          */
         set
-          .filter(({ bundle }) => !bundle)
+          .filter(({ name }) => name.substring(0, serverScriptPrefix.length) === serverScriptPrefix)
           .forEach(s => {
             s.triggers
               .filter(({ eventTypes = [] }) => eventTypes.includes('onManual'))
@@ -34,7 +42,7 @@ export default {
                 // to modify trigger we're passing to it by adding script name
                 t.scriptName = s.name
                 try {
-                  this.$EventBus.Register(ev => serverScriptHandler(api, ev, s.name), t)
+                  this.$EventBus.Register(ev => serverScriptHandler(ev, s.name), t)
                 } catch (e) {
                   console.error(e)
                 }
@@ -43,9 +51,21 @@ export default {
       }
     },
 
+    /**
+     * Loads bundle from system API and registers
+     * @return {Promise<T>}
+     */
     // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    loadClientScriptBundle ({ bundle, type = 'scripts', ext = 'js', verbose = false } = {}) {
+    loadBundle ({ bundle, type = 'client-scripts', ext = 'js', verbose = false, ctx = undefined } = {}) {
       const ep = this.$SystemAPI.automationBundleEndpoint({ bundle, type, ext })
+
+      if (ctx === undefined) {
+        throw new Error('can not load bundle and register scripts without context')
+      }
+
+      if (typeof ctx.withArgs !== 'function') {
+        throw new Error('invalid context object, expecting withArgs function')
+      }
 
       return this.$SystemAPI.api().get(ep)
         .then(({ data }) => {
@@ -56,12 +76,25 @@ export default {
             return
           }
 
+          if (verbose) {
+            console.debug('bundle loaded', { bundle, type, ext })
+          }
+
+          // eval loaded bundle
           // eslint-disable-next-line no-new-func
           (new Function(data))()
-          window.composeClientScripts.Register({
+
+          // call script registrator on bundle
+          window[`${bundle}ClientScripts`].Register({
             verbose,
             eventbus: this.$EventBus,
             uiHooks: this.UIHooks,
+
+            // Generic event handler
+            exec: (script, ev) => {
+              const args = new corredor.Args(ev.args)
+              corredor.Exec(script, args, ctx.withArgs(args))
+            },
           })
         })
         .catch(({ message }) => {
