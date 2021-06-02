ceae33ff2961bb157ca978f5026a3ac1d7be6423
Mia Arh <mia.arh@crust.tech>
2021-06-02 15:28:11 +0200

    Make topbar buttons rounded and include version tag in help dropdown

--- src/components/navigation/CTopbar.vue
+++ src/components/navigation/CTopbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="header-navigation d-flex align-items-center justify-content-between position-absolute sticky-top">
+  <div class="header-navigation d-flex align-items-center justify-content-between sticky-top">
     <div
       class="d-flex text-nowrap flex-grow-1 align-items-center header px-2"
     >
@@ -16,17 +16,22 @@
       >
         <slot name="title" />
       </h2>
-      <b-form-input
-        type="text"
-        name="search"
-        placeholder="Search..."
-        class="ml-3"
-      />
-
+      <b-btn
+          variant="outline-light"
+          to="/"
+          size="lg"
+          class="text-dark border-0 nav-icon rounded-circle p-2"
+      >
+        <font-awesome-icon
+            class="m-0 h5"
+            :icon="['fas', 'grip-horizontal']"
+        />
+      </b-btn>
       <b-dropdown
         size="lg"
         variant="outline-light"
-        toggle-class="text-decoration-none text-dark border-0"
+        class="nav-icon mx-1"
+        toggle-class="text-decoration-none text-dark rounded-circle border-0 p-2"
         menu-class="border-0 shadow-sm text-dark font-weight-bold mt-2"
         right
         no-caret
@@ -58,12 +63,21 @@
         >
           {{ $t('navigation.help.feedback') }}
         </b-dropdown-item>
+        <b-dropdown-divider />
+        <b-dropdown-item
+            disabled
+            class="small"
+        >
+          {{ $t('navigation.help.version') }}
+          <br>
+          {{ frontendVersion }}
+        </b-dropdown-item>
       </b-dropdown>
-
       <b-dropdown
         size="lg"
         variant="outline-light"
-        toggle-class="text-decoration-none text-dark border-0"
+        class="nav-user-icon"
+        toggle-class="nav-icon p-0 text-decoration-none text-dark rounded-circle border"
         menu-class="border-0 shadow-sm text-dark font-weight-bold mt-2"
         right
         no-caret
@@ -71,7 +85,7 @@
         <template #button-content>
           <font-awesome-icon
             class="m-0 h5"
-            :icon="['fas', 'user-cog']"
+            :icon="['far', 'user']"
           />
           <span class="sr-only">
             {{ $t('navigation.help.forum') }}
@@ -92,24 +106,15 @@
         >
           {{ $t('navigation.userSettings.changePassword') }}
         </b-dropdown-item>
+        <b-dropdown-divider />
         <b-dropdown-item
           href=""
           @click="$auth.logout()"
+          class="mt-2"
         >
           {{ $t('navigation.userSettings.logout') }}
         </b-dropdown-item>
       </b-dropdown>
-      <b-btn
-        variant="outline-light"
-        to="#"
-        size="lg"
-        class="text-dark border-0"
-      >
-        <font-awesome-icon
-          class="m-0 h5"
-          :icon="['fas', 'grip-horizontal']"
-        />
-      </b-btn>
     </div>
   </div>
 </template>
@@ -145,6 +150,11 @@ export default {
       const [year, month] = VERSION.split('.')
       return `https://docs.cortezaproject.org/corteza-docs/${year}.${month}/index.html`
     },
+
+    frontendVersion () {
+      /* eslint-disable no-undef */
+      return VERSION
+    },
   }
 }
 </script>
@@ -152,26 +162,23 @@ export default {
 <style lang="scss" scoped>
 $header-height: 64px;
 $nav-width: 320px;
-
-input[type=text] {
-  width: 90px;
-  -webkit-transition: width 0.4s ease-in-out;
-  transition: width 0.4s ease-in-out;
-  background: transparent;
-  border: none;
-
-  /* When the input field gets focus, change its width to 100% */
-  &:focus {
-    width: 100%;
-    background: white;
-    border: 2px solid #4D7281;
-  }
-}
+$nav-icon-size: 40px;
+$nav-user-icon-size: 50px;
 
 .icon-logo {
   height: calc(#{$header-height} / 2);
   background-repeat: no-repeat;
   background-position: center;
+}
+
+.nav-icon {
+  width: $nav-icon-size;
+  height: $nav-icon-size;
+}
+
+.nav-user-icon {
+  width: $nav-user-icon-size;
+  height: $nav-user-icon-size;
 }
 
 .header-navigation {
