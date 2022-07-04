2094e2a2d9eea1b940312ce8c22257eea452e1e0
Katrin Yordanova <katrin.yordanova@crust.tech>
2022-07-04 20:08:25 +0300

    Add new echart component

---
+++ src/components/reporter/chart/Chart.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-chart
+    class="position-relative h-100 w-100 p-2 bg-white"
+    :option="option"
+  />
+</template>
+
+<script>
+import * as echarts from 'echarts'
+import VChart from 'vue-echarts'
+
+export default {
+  name: 'Chart',
+
+  components: {
+    echarts,
+    VChart,
+  },
+
+  props: {
+    option: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
