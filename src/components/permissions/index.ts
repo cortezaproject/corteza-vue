7986de596aec30bda2be4d490564d25a713b2eb9
Jože Fortun <joze.fortun@gmail.com>
2022-07-11 18:06:21 +0200

    Rework permissions

--- src/components/permissions/index.ts
+++ src/components/permissions/index.ts
@@ -1,3 +1,2 @@
 export { default as CPermissionsButton } from './CPermissionsButton.vue'
-export { default as CPermissionsForm } from './CPermissionsForm.vue'
 export { default as CPermissionsModal } from './CPermissionsModal.vue'
