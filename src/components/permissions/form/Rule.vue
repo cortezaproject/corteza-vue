d47f371d7a605d97b86bd32ca1636dd65873c0fc
Jože Fortun <joze.fortun@gmail.com>
2022-07-11 18:06:21 +0200

    Rework permissions

--- src/components/permissions/form/Rule.vue
+++ src/components/permissions/form/Rule.vue
@@ -1,41 +1,20 @@
 <template>
-  <b-row
-    no-gutters
-    class="mb-lg-1"
-  >
-    <b-col
-      cols="12"
-      lg="6"
-      order="2"
-      order-lg="1"
+  <div>
+    <p
+      :title="title || `${operation} on ${resource}`"
+      class="mb-1"
     >
-      <access
-        :access="access"
-        :current="current"
-        :enabled="enabled"
-        @update="onUpdate"
-      />
+      {{ title || `${operation} on ${resource}` }}
+    </p>
 
-      <b-button
-        v-show="isChanged"
-        variant="link"
-        class="pl-0"
-        @click="onReset"
-      >
-        {{ $t('ui.reset-back', {current}) }}
-      </b-button>
-    </b-col>
-    <b-col
-      cols="12"
-      lg="6"
-      class="mb-lg-4 text-wrap"
-      order="1"
-      order-lg="2"
-    >
-      <b v-html="title || `${operation} on ${resource}`" />
-      <div>{{ description || '&nbsp;' }}</div>
-    </b-col>
-  </b-row>
+    <access
+      :access="access"
+      :current="current"
+      :enabled="enabled"
+      class="w-100"
+      @update="onUpdate"
+    />
+  </div>
 </template>
 <script lang="js">
 import Access from './Access.vue'
